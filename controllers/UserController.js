853e7984f07a1648e675a84535c37a25246d1598
rajeev_ <rajeev.kumar@codefire.in>
2024-05-27 19:42:59 +0530

    added create oparation for users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,7 @@ const UserSchema = db.users;
 
 
 
-const getAllUsers = async (req, res ) => {
+const getAllUsers = async (reqs, res ) => {
     try{
         const users = await UserSchema.findAll();
         res.status(200).json({
@@ -18,9 +18,9 @@ const getAllUsers = async (req, res ) => {
     }
 }
 
-const createUser = async (req, res ) => {
+const createUser = async (reqs, res ) => {
     try{
-        const {user_name,user_email,user_mobile,password,dob} = req.body;
+        const {user_name,user_email,user_mobile,password,dob} = reqs.body;
         const users = await UserSchema.create({
             user_name : user_name,
             user_email : user_email,
@@ -45,5 +45,27 @@ const createUser = async (req, res ) => {
     }
 }
 
+const getUserDetail = async (reqs, res) => {
+    try{
 
-module.exports = {getAllUsers,createUser}
+        // reqs.body
+        //reqs.param
+        // reqs.query
+        const userId = reqs.params.id;
+        console.log('userIdDD',reqs.params.id)
+        const userData = await UserSchema.findOne({
+            where: {
+                id:  userId   
+            }
+        });
+        res.status(200).json({
+            'status' :'success',
+            'status_code':200,
+            'data':userData
+        });
+    }catch(e){
+        console.log('error while getting user detaills',e)
+    }
+}
+
+module.exports = {getAllUsers,createUser,getUserDetail}
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const  {getAllUsers}  =  require("../controllers/UserController");
+const  {getAllUsers,getUserDetail,createUser}  =  require("../controllers/UserController");
 
 router.get("/",getAllUsers);
 
-router.route("/:id").get((req, res) => {
-    return res.json({
-        'status' : 'success',
-        'status_code' : 20,
-        'message' : 'user get successfully'
-    });
-});
 
-router.route("/").post((req, res) => {
-    return res.json({
-        'status' : 'success',
-        'status_code' : 20,
-        'message' : 'user creates successsfully'
-    });
-});
+router.get("/:id",getUserDetail);
+
+router.post("/",createUser);
 
 router.route("/:id").delete((req, res) => {
     return res.json({
